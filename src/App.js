139f7516dbb82761b3035e9c912caeffc0c28822
kanounfoued <=>
2021-02-20 15:08:10 +0100

    :lipstick: add some component for test in the app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import img from './asset/LD.png';
+
+// import './style.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className="app">This is my app.</div>
+      {/* <img src={img} alt="img" /> */}
+    </div>
+  );
+};
+
+export default App;
